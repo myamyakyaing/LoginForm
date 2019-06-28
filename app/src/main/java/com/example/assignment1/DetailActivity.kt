321417ca4806986d3b62cdfbc666185ac4554772
Mya Mya Kyaing <myamyakyaing.mmk1996@gmail.com>
2019-06-28 13:05:08 +0630

    Assignment1 Finish

--- app/src/main/java/com/example/assignment1/DetailActivity.kt
+++ app/src/main/java/com/example/assignment1/DetailActivity.kt
@@ -7,11 +7,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class DetailActivity : AppCompatActivity() {
-
+    val REQUEST_IMAGE_GET = 1
     companion object {
         fun newIntent(context: Context): Intent {
             val intent = Intent(context, DetailActivity::class.java)
@@ -22,10 +23,37 @@ class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        imgSearch.setOnClickListener {
+            var data = edtSearch.text.toString()
+            val browserIntent = Intent(Intent.ACTION_WEB_SEARCH).apply {
+                Uri.parse(data)
+            }
+
+            startActivity(browserIntent)
+        }
+
         btnPhone.setOnClickListener {
             val callPhone = Intent(Intent.ACTION_DIAL)
             callPhone.setData(Uri.parse("tel:${edtPhone.text.toString()}"))
             startActivity(callPhone)
+        }
+         btnCamera.setOnClickListener {
+             val intent = Intent(Intent.ACTION_GET_CONTENT)
+             intent.type = "image/*"
+             if (intent.resolveActivity(packageManager) != null) {
+                 startActivityForResult(intent, REQUEST_IMAGE_GET)
+             }
+         }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_GET && resultCode == RESULT_OK) {
+            val fullPhotoUri: Uri = data!!.data!!
+            Glide.with(this).load(fullPhotoUri).into(ivProfileImage)
+
         }
     }
 }
